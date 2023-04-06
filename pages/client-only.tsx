83389bcbdda5776a42071f901680684aeb40a82f
Hamed Ghazali <hamedghazali53@gmail.com>
2023-04-06 19:50:28 +0330

    remove header from client-only

--- pages/client-only.tsx
+++ pages/client-only.tsx
@@ -1,13 +1,12 @@
-import { Layout, Header, InfoBox, PostList } from '@/components'
+import { InfoBox, PostList } from "@/components";
 
 const ClientOnly = () => {
   return (
-    <Layout>
-      <Header />
+    <>
       <InfoBox>ℹ️ This data is loaded on client and not prefetched</InfoBox>
       <PostList />
-    </Layout>
-  )
-}
+    </>
+  );
+};
 
-export default ClientOnly
+export default ClientOnly;
