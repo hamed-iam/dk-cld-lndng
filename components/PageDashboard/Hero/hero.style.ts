1f60d8185e9eddbc30e5e441119f37f49eb11b13
Hamed Ghazali <hamedghazali53@gmail.com>
2023-04-20 12:53:05 +0330

    hero section dashboard in progress

--- components/PageDashboard/Hero/hero.style.ts
+++ components/PageDashboard/Hero/hero.style.ts
@@ -4,12 +4,80 @@ import { ThemeStyledProps } from "@/styles/themes";
 export default styled.aside(
   ({
     theme: {
-      colors: { blue, background },
+      colors: { LightNatural100, LightNatural400, LightThemePrimary600 },
     },
   }: ThemeStyledProps) => {
     return css`
-      border: 1px solid red;
       min-height: 663px;
+      padding: calc(24px + ((130 - 24) * (100vw - 320px) / (1920 - 320)));
+      display: flex;
+      justify-content: space-around;
+      flex-wrap: wrap;
+      .title-wrapper {
+        &-super {
+          font-weight: 400;
+          font-size: 14px;
+          color: ${LightNatural100};
+        }
+        &-slogan {
+          position: relative;
+          padding: 4px;
+          span {
+          }
+          .icon {
+            width: 9px;
+            position: absolute;
+            &.icon-cornerTLIcon {
+              top: 0;
+              left: 0;
+            }
+            &.icon-cornerTRIcon {
+              top: 0;
+              right: 0;
+            }
+            &.icon-cornerBLIcon {
+              bottom: 0;
+              left: 0;
+            }
+            &.icon-cornerBRIcon {
+              bottom: 0;
+              right: 0;
+            }
+
+            &.icon-sparkleIcon {
+              position: relative;
+              width: 56px;
+            }
+          }
+          h1 {
+            font-style: normal;
+            font-weight: 700;
+            margin: 0;
+            color: ${LightNatural400};
+            .better {
+              color: linear-gradient(90deg, rgba(203,95,242,1) 100%, rgba(255,90,130,1) 100%);
+            }
+            .infrastructure {
+              color: ${LightThemePrimary600};
+            }
+            .sparkle {
+              display: flex;
+            }
+            font-size: calc(
+              40px + (64 - 40) * ((100vw - 320px) / (1920 - 320))
+            );
+          }
+        }
+        &-sub {
+        }
+      }
+
+      .image-wrapper {
+        .icon {
+          max-width: 383px;
+          min-width: 311px;
+        }
+      }
     `;
   }
 );
--- components/PageDashboard/Hero/index.tsx
+++ components/PageDashboard/Hero/index.tsx
@@ -1,20 +1,37 @@
 import { useTranslation } from "next-i18next";
-import StyledWrapper from './hero.style'
-
+import StyledWrapper from "./hero.style";
+import SvgIcon from "@/components/SvgIcon";
 
 const HeroCmp = () => {
   const { t } = useTranslation("dashboard");
 
   return (
     <StyledWrapper>
-      <div>
-        <p>{t("hero.super-h1")}</p>
+      <div className="title-wrapper">
+        <p className="title-wrapper-super">{t("hero.super-h1")}</p>
+        <div className="title-wrapper-slogan">
+          <SvgIcon title="cornerTLIcon" viewBox="0 0 10 10" />
+          <SvgIcon title="cornerTRIcon" viewBox="0 0 10 10" />
+          <section>
+            {/* <h1>{t("hero.h1")}</h1> */}
+            <h1>
+              Let&apos;s build
+              <br />a <span className="better">Better</span>{" "}
+              <span className="infrastructure">Infrastructure</span>,
+              <br />
+              <span className="sparkle">
+                together <SvgIcon title="sparkleIcon" viewBox="0 0 56 57" />
+              </span>
+            </h1>
+          </section>
+          <SvgIcon title="cornerBLIcon" viewBox="0 0 10 10" />
+          <SvgIcon title="cornerBRIcon" viewBox="0 0 10 10" />
+        </div>
+        <p className="title-wrapper-sub">{t("hero.sub-h1")}</p>
       </div>
-      <div>
-        <h1>{t("hero.h1")}</h1>
-      </div>
-      <div>
-        <p>{t("hero.sub-h1")}</p>
+
+      <div className="image-wrapper">
+        <SvgIcon title="heroCloudIcon" viewBox="0 0 384 343" />
       </div>
     </StyledWrapper>
   );
