457d4800548d56c236af464ba21086c95d80bdbb
Ali Izadi <ali.izadi@digikala.com>
2023-07-09 11:31:13 +0330

    DIG-466, EdgeService Page | Pricing Section

---
+++ components/PageEdgeServices/EdgePricingSection/EdgePricingCard.tsx
@@ -0,0 +1,83 @@
+import SvgIcon from "@/components/SvgIcon";
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+import { EdgePricingCardStyledWrapper } from "./edgePricingSection.style";
+
+export type Item = {
+  title: string;
+  status: boolean;
+};
+
+interface CloudBenefitCardPropTypes {
+  title: string;
+  subtitle: string;
+  items: Array<Item> | never[];
+  background: string;
+}
+
+export default function EdgePricingCard({
+  title,
+  subtitle,
+  items,
+  background,
+}: CloudBenefitCardPropTypes) {
+  const [isActive, setIsActive] = useState(false);
+  const [height, setHeight] = useState(150);
+  const { t } = useTranslation("edge-services");
+
+  return (
+    <EdgePricingCardStyledWrapper
+      backgroundImage={background}
+      active={isActive}
+    >
+      <div className="container">
+        <input
+          type="checkbox"
+          className="view-more-state"
+          checked={isActive}
+          onChange={() => {
+            setIsActive(!isActive);
+          }}
+          style={{ display: "none" }}
+        />
+        <div className="header">
+          <h1>{title}</h1>
+          <h4>{subtitle}</h4>
+        </div>
+        <div className="content">
+          {/* <div className={isActive ? "content expand" : "content collapse"}> */}
+          {items.map((item, index) => {
+            return (
+              <div key={index} className="content-item">
+                {item.status ? (
+                  <SvgIcon title="okIcon" viewBox="0 0 16 16" />
+                ) : (
+                  <SvgIcon title="notOkIcon" viewBox="0 0 16 16" />
+                )}
+                <p>{item.title}</p>
+              </div>
+            );
+          })}
+        </div>
+        <div
+          onClick={() => {
+            setIsActive(!isActive);
+            setHeight(isActive ? 200 : 150);
+          }}
+          className={
+            isActive
+              ? "view-button view-button-expand"
+              : "view-button view-button-collapse"
+          }
+        >
+          {isActive ? (
+            <p>{t("pricing.action.view-less")}</p>
+          ) : (
+            <p>{t("pricing.action.view-more")}</p>
+          )}
+          <SvgIcon title="edgeFeatureArrowIcon" viewBox="0 0 18 18" />
+        </div>
+      </div>
+    </EdgePricingCardStyledWrapper>
+  );
+}
